9af6fc65676eeaf33d8c0b651e91d7eabc532fac
Kenny <khuong.pham@2359media.com>
2022-11-28 22:48:38 +0700

    Add Firebase dependencies

---
+++ SwiftUI_google_cloud_video_intelligence/Constants.swift
@@ -0,0 +1,12 @@
+//
+//  Constants.swift
+//  SwiftUI_google_cloud_video_intelligence
+//
+//  Created by Khuong Pham on 28/11/2022.
+//
+
+import Foundation
+
+struct Constants {
+    static let API_KEY = "AIzaSyBd3EqeQwl1nmqRgCuzQvxAfJdhxn-P1YQ"
+}
