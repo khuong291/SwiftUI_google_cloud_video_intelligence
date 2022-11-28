6b240954c2f5f4470d8d59d33404e5bb8c193851
Kenny <khuong.pham@2359media.com>
2022-11-28 22:36:36 +0700

    Initial Commit

---
+++ SwiftUI_google_cloud_video_intelligence/SwiftUI_google_cloud_video_intelligenceApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_google_cloud_video_intelligenceApp.swift
+//  SwiftUI_google_cloud_video_intelligence
+//
+//  Created by Khuong Pham on 28/11/2022.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_google_cloud_video_intelligenceApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
